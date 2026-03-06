99f9fb050697d6f4388d787ecc8a2a0cad9aa1b0
Prabhat Mishra <workforprabhat1254@gmail.com>
2026-03-06 21:51:44 +0530

    updated: blogs section in landing

--- app/_components/BlogSection.tsx
+++ app/_components/BlogSection.tsx
@@ -2,25 +2,30 @@ import { Button } from "@/components/ui/button";
 import { getAllBlogs } from "@/lib/blogs";
 import Link from "next/link";
 import BlogItemCard from "../blogs/_components/BlogItemCard";
-import FloatingIcons from "./FloatingIcons";
 
 export default function BlogSection() {
     const blogs = getAllBlogs(3);
     return (
-        <div className="my-10 md:my-20 grid grid-cols-1 lg:grid-cols-[auto_minmax(300px,1fr)] gap-6">
-            <div>
-                <h3 className="text-2xl font-light">Blogs</h3>
-                <div className={`flex flex-col gap-6 my-6`}>
-                    {blogs.map((post) => (
-                        <BlogItemCard key={post.slug} post={post} />
-                    ))}
-                </div>
+        <div className="my-10 md:my-20 grid grid-cols-1">
+            <h3 className="text-2xl font-light">Blogs</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-6">
+                {blogs.map((post, index) => (
+                    <BlogItemCard
+                        key={post.slug}
+                        post={post}
+                        linkClassName={
+                            index === blogs.length - 1
+                                ? "md:col-span-2 md:h-full lg:col-span-1"
+                                : ""
+                        }
+                    />
+                ))}
+            </div>
+            <div className="flex items-center justify-center">
                 <Link href="/blogs">
                     <Button variant={"outline"}>More Blogs</Button>
                 </Link>
             </div>
-
-            <FloatingIcons />
         </div>
     );
 }
--- app/blogs/_components/BlogItemCard.tsx
+++ app/blogs/_components/BlogItemCard.tsx
@@ -1,45 +1,73 @@
 "use client";
 
 import { Badge } from "@/components/ui/badge";
+import {
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 import type { ListOfBlogPostsItem } from "@/lib/blogs/types";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
 
 export default function BlogItemCard({
     post,
     onTagClick,
+    className,
+    linkClassName
 }: {
     post: ListOfBlogPostsItem;
     onTagClick?: (tag: string | null) => void;
+    className?: string
+    linkClassName?: string
 }) {
     const url = `/blogs/${post.slug}`;
     return (
-        <div>
-            <Link href={url}>
-                <h3 className="text-lg font-medium">{post.metadata.title}</h3>
-                <div className="flex flex-wrap gap-2 items-center text-muted-foreground text-sm">
-                    <span>
-                        {post.metadata.date
-                            ? new Date(post.metadata.date).toLocaleDateString()
-                            : ""}
-                    </span>
-                    <span>•</span>
-                    <span>{post.metadata.author?.join(", ")}</span>
-                    <span>•</span>
-                    <span className="italic">{post.metadata.readTime}</span>
-                </div>
-            </Link>
-            <div className="flex flex-wrap gap-2 mt-1">
-                {post.metadata.tags?.map((tag) => (
-                    <Badge
-                        key={tag}
-                        className={onTagClick ? "cursor-pointer" : ""}
-                        onClick={() => onTagClick?.(tag)}
-                        variant={"outline"}
-                    >
-                        {tag}
-                    </Badge>
-                ))}
-            </div>
-        </div>
+        <Link href={url} className={cn("group h-full", linkClassName)}>
+            <Card className={cn("h-full shadow-none group-hover:shadow-md group-hover:scale-[101%] transition-all duration-300 hover:border-border", className)}>
+                <CardHeader className="pb-2">
+                    <CardTitle className="text-lg leading-tight">
+                        {post.metadata.title}
+                    </CardTitle>
+                    <CardDescription className="space-x-1">
+                        <span>{post.metadata.description}</span>
+                        {post.metadata.description && (
+                            <span className="text-sm text-muted-foreground italic underline group-hover:text-blue-500">
+                                Read more
+                            </span>
+                        )}
+                    </CardDescription>
+                    <CardDescription className="flex items-center gap-1 text-muted-foreground text-sm">
+                        <span>
+                            {post.metadata.date
+                                ? new Date(
+                                      post.metadata.date,
+                                  ).toLocaleDateString()
+                                : ""}
+                        </span>
+                        <span>•</span>
+                        <span>{post.metadata.author?.join(", ")}</span>
+                        <span>•</span>
+                        <span className="italic">{post.metadata.readTime}</span>
+                    </CardDescription>
+                    <div className="flex flex-wrap gap-1.5">
+                        {post.metadata.tags?.map((tag) => (
+                            <Badge
+                                key={tag}
+                                className={onTagClick ? "cursor-pointer" : ""}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    onTagClick?.(tag);
+                                }}
+                                variant={"outline"}
+                            >
+                                {tag}
+                            </Badge>
+                        ))}
+                    </div>
+                </CardHeader>
+            </Card>
+        </Link>
     );
 }
