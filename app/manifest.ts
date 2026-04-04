f545eedef79f92f7de4e98a8740682b729c99c3f
Prabhat Mishra <workforprabhat1254@gmail.com>
2026-04-04 14:06:55 +0530

    added manifets for install

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,15 +6,15 @@ export default function manifest(): MetadataRoute.Manifest {
         name: mainData.title,
         short_name: "Blade Tools",
         description: mainData.description,
-        start_url: "https://localhost:3000",
+        start_url: "/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#000000",
         icons: [
             {
-                src: "/favicon.ico",
-                sizes: "48x48",
-                type: "image/x-icon",
+                src: "/logo-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
             },
         ],
     };
--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import FAQSection from "./_components/FAQSection";
 import HeroSection from "./_components/HeroSection";
 import { racingSansOne } from "./fonts";
 import { DiMootoolsBadge } from "react-icons/di";
+import { RiKnifeLine } from "react-icons/ri";
 
 export default function Home() {
     return (
@@ -15,15 +16,6 @@ export default function Home() {
             <CategorySection />
             <FAQSection />
             <BlogSection />
-
-            <div className="flex items-center justify-center p-8">
-                <div
-                    className={`${racingSansOne.className} overflow-hidden relative border border-border p-2 size-15 bg-black flex flex-col items-center justify-center`}
-                >
-                    <span className="w-15 h-6 bg-red-500"></span>
-                    <DiMootoolsBadge className="absolute mt-20 size-45" />
-                </div>
-            </div>
         </LayoutWrapper>
     );
 }
